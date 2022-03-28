e206a0bce5bc7514410b8c5e5382d5a9c9f1dbdf
glynn29 <glynn_29@hotmail.com>
2022-03-27 21:41:43 -0500

    add better UI and oder again feature

--- src/containers/Pages/Client/OrderHistory/OrderHistory.js
+++ src/containers/Pages/Client/OrderHistory/OrderHistory.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import { withSnackbar } from "notistack";
 
 import Container from "@material-ui/core/Container";
-
-import useFetch from "../../../../apiCalls/useFetch";
 import Grid from "@material-ui/core/Grid";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import Typography from "@material-ui/core/Typography";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Button from "@material-ui/core/Button";
+import Slide from "@material-ui/core/Slide";
 
 import { firestore } from "../../../../firebase"
-import Item from "../../Admin/Orders/Forms/Item";
 import formStyles from "../../../../components/UI/Styles/formStyle";
+import * as actions from "../../../../store/actions";
 
 const OrderHistory = (props) => {
     const styles = formStyles();
-    // const [result, isLoading] = useFetch(ordersRef);
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
@@ -32,18 +37,63 @@ const OrderHistory = (props) => {
             .catch();
     }, [props.email]);
 
+    const handleOrderAgain = (order, event) => {
+        event.stopPropagation();
+        order.items.forEach(item => {
+            props.onAddItem(item, item.amount);
+        });
+        props.enqueueSnackbar('Items added to your cart successfully', {
+            variant: "success",
+            autoHideDuration: 3000,
+            TransitionComponent: Slide,
+            anchorOrigin: {vertical: 'bottom', horizontal: 'right'}
+        });
+    };
+
     return (
         <Container className={styles.Container}>
-            {/*//TODO get only order for that user, Create item card for display*/}
-            Still under construction...
-            <ol>
-                <Container style={{maxHeight: '40em', overflow: 'auto'}}>
-                    {orders.map(order => <li key={order.id}><Grid container spacing={2}>{order.items.map(
-                        item => <Grid item xs={12} key={item.id}><Item item={item}/></Grid>)}<br/></Grid></li>)}
-                </Container>
-            </ol>
+            {
+                orders.map((order, index) =>
+                               <div key={order.id}>
+                                   <Accordion style={{alignContent: 'center'}}>
+                                       <AccordionSummary
+                                           expandIcon={<ExpandMoreIcon/>}
+                                       >
+                                           <div style={{
+                                               justifyContent: 'space-between',
+                                               width: '100%',
+                                               display: 'flex'
+                                           }}>
+                                               <Typography
+                                                   style={{alignSelf: 'center'}}>Order
+                                                   #{index + 1}: {order.date}</Typography>
+                                               <Button onClick={(event) => {
+                                                   handleOrderAgain(order, event);
+                                               }}>
+                                                   Order Again
+                                               </Button>
+                                           </div>
+                                       </AccordionSummary>
+                                       <AccordionDetails>
+                                           <Grid container>
+                                               {order.items.map(
+                                                   item => <Grid item xs={12} sm={6} md={4} key={item.name}>{item.name}
+                                                       <span style={{fontWeight: 'bold'}}>X</span> {item.amount}</Grid>
+                                               )}
+                                           </Grid>
+                                       </AccordionDetails>
+                                   </Accordion>
+                               </div>
+                )
+            }
         </Container>
     );
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddItem: (item, amount) => dispatch(actions.onAddItem(item, amount)),
+    }
 };
 
 const mapStateToProps = state => {
@@ -52,4 +102,4 @@ const mapStateToProps = state => {
     };
 };
 
-export default connect(mapStateToProps)(OrderHistory);
+export default connect(mapStateToProps, mapDispatchToProps)(withSnackbar(OrderHistory));
