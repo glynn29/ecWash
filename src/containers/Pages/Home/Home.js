7c8d031bdb903835c35a3a27ae8da806f0724d5d
glynn29 <glynn_29@hotmail.com>
2022-04-11 22:01:39 -0500

    add home image and format

--- src/containers/Pages/Home/Home.js
+++ src/containers/Pages/Home/Home.js
@@ -1,45 +1,26 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import { withSnackbar } from "notistack";
+import React from "react";
 
 import Typography from "@material-ui/core/Typography";
-import Slide from "@material-ui/core/Slide";
 
 import * as classes from './Home.module.css';
-import * as actions from "../../../store/actions";
+import useStyles from "../../../components/UI/Styles/formStyle";
 
-const Home = (props) => {
-    useEffect(() => {
-        if (props.orderComplete) {
-            props.enqueueSnackbar('Order Complete', {
-                variant: "success",
-                autoHideDuration: 9000,
-                TransitionComponent: Slide,
-                anchorOrigin: {vertical: 'bottom', horizontal: 'right'}
-            });
-            props.clearOrder();
-        }
-    }, [props.orderComplete]);
+const Home = () => {
+    const styles = useStyles();
 
     return (
-        <div className={classes.Box}>
-            <Typography variant="h2" className={classes.Line}><b>EC Wash </b></Typography>
-            <Typography variant="h4" className={classes.Line}><b>Landing Page info here</b></Typography>
-            <br/>
+        <div style={{marginTop: '-20px'}}>
+            <div className={classes.Hero}/>
+            <div style={{textAlign: 'center'}}>
+                <Typography className={styles.homeTitle}>
+                    Express Car Wash Equipment
+                </Typography>
+                <Typography className={styles.homeTagLine}>
+                    Your Car Wash Experience Made EC
+                </Typography>
+            </div>
         </div>
     );
 };
 
-const mapStateToProps = state => {
-    return {
-        orderComplete: state.cart.orderComplete
-    };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        clearOrder: () => dispatch(actions.clearOrder()),
-    }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(withSnackbar(Home));
+export default Home;
