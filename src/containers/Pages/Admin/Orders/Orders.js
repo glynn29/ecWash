7a8158123c9eb29e5367197a7f9d7fca228f1fe6
glynn29 <glynn_29@hotmail.com>
2022-04-16 20:41:24 -0500

    use usefetch , format

--- src/containers/Pages/Admin/Orders/Orders.js
+++ src/containers/Pages/Admin/Orders/Orders.js
@@ -7,76 +7,105 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
 import TextField from "@material-ui/core/TextField";
 import AutoComplete from "@material-ui/lab/Autocomplete";
-import Spinner from "../../../../components/UI/Spinner/Spinner";
+import Skeleton from "@material-ui/lab/Skeleton";
 
 import { firestore } from "../../../../firebase";
 import OrdersTable from "./OrdersTable";
 import useStyles from "../../../../components/UI/Styles/formStyle";
+import useFetch from "../../../../apiCalls/useFetch";
 
 const statusList = [
     'new',
+    'sold',
     'delivered',
-    'billed',
-    'paid'
+    'invoiced'
 ];
 
 const Orders = () => {
-    const styles= useStyles();
+    const styles = useStyles();
     const [tableData, setTableData] = useState([]);
     const [filterActive, setFilterActive] = useState(false);
     const [filterValue, setFilterValue] = useState(null);
     const [filteredTableData, setFilteredTableData] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
 
-    async function getOrders() {
-        const orders = [];
-        setIsLoading(true);
-        const ordersRef =  firestore.collection("orders")
-            .orderBy("date", "desc");
-        const data = await ordersRef.get();
-        data.forEach(order => {
-            const num = Math.floor(Math.random() * 4);
-            orders.push({...order.data(), id: order.id, status: statusList[num], statusStep: num});
-        });
-        setTableData(orders);
-        setIsLoading(false);
-    }
+    //TODO order by time
+    const ordersRef = firestore.collection("orders")
+        .orderBy("date", "desc")
+        .orderBy("time", "desc");
+    const [result, isLoading] = useFetch(ordersRef);
 
     useEffect(() => {
-        getOrders()
-            .catch(error => console.log(error));
-    }, []);
+        setTableData(result);
+    }, [result]);
 
     useEffect(() => {
         if (filterValue && filterActive) {
             filterOrders();
         }
-    },[filterValue, filterActive]);
+    }, [filterValue, filterActive, tableData]);
 
     const filterOrders = () => {
-        let filteredOrders = tableData.filter((order) => order.name === filterValue);
+        let filteredOrders = tableData.filter((order) => order.nickName === filterValue);
         setFilteredTableData(filteredOrders);
     };
 
-    const handleFilterSwitch =() => {
+    const handleFilterSwitch = () => {
         setFilterActive(!filterActive);
     };
 
-    if(isLoading) return <Spinner />;
+    const onEdit = (order, id) => {
+        const tempOrder = {...order};
+        delete tempOrder.id;
+        firestore.collection("orders")
+            .doc(id)
+            .set(tempOrder, {merge: true})
+            .then(() => {
+                let tempTableData = [...tableData];
+                const index = tableData.findIndex((row) => row.id === id);
+                tempTableData[index] = order;
+                setTableData(tempTableData);
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    };
+
+    const onDelete = (id) => {
+        firestore.collection("orders")
+            .doc(id)
+            .delete()
+            .then(() => {
+                let tempTableData = tableData.filter((row) => row.id !== id);
+                setTableData(tempTableData);
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    };
+
+    if (isLoading) {
+        return (
+            <Container>
+                <Skeleton animation={"wave"} variant={"rect"} width={'100%'} height={400}/>
+            </Container>
+        );
+    }
+
     return (
         <Container>
-            <div style={{display: 'flex', justifyContent: 'center'}} >
+            <div style={{display: 'flex', justifyContent: 'center'}}>
                 <div className={styles.searchBar}>
                     <FormControl className={styles.searchInput}>
                         <AutoComplete
                             freeSolo
                             onChange={(event, value) => {
-                                setFilterValue(value ? value.name : null);
+                                setFilterValue(value);
                             }}
-                            options={tableData.sort((a, b) => -b.name.charAt(0)
-                                .localeCompare(a.name.charAt(0)))}
-                            groupBy={(option) => option.name.charAt(0)}
-                            getOptionLabel={(option) => (option.name)}
+                            options={Array.from(new Set(tableData.sort((a, b) => -b.nickName.charAt(0)
+                                .localeCompare(a.nickName.charAt(0)))
+                                                            .map(row => row.nickName)))}
+                            groupBy={(option) => option.charAt(0)}
+                            getOptionLabel={(option) => (option)}
                             renderInput={(params) => (
                                 <TextField {...params} key={params} variant="outlined" placeholder="Filter By NickName"/>
                             )}
@@ -87,17 +116,18 @@ const Orders = () => {
                 <FormControlLabel
                     disabled={filterValue === null}
                     style={{float: 'right'}}
-                    control={<Switch checked={filterActive} onChange={handleFilterSwitch} />}
+                    control={<Switch checked={filterActive} onChange={handleFilterSwitch}/>}
                     label={filterActive ? "Disable Filter" : "Enable Filter"}
                 />
             </div>
-            <br />
-            <Divider />
-            <br />
+            <br/>
+            <Divider/>
+            <br/>
             <OrdersTable
-                filter={(filterValue && filterActive)}
                 tableData={(filterValue && filterActive) ? filteredTableData : tableData}
-                filteredTableData={filteredTableData} />
+                statusList={statusList}
+                onEdit={onEdit}
+                onDelete={onDelete}/>
         </Container>
     );
 };
