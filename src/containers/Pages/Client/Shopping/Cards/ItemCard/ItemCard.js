2757e4e801876b0f324e787d3cad6e69272bf466
glynn29 <glynn_29@hotmail.com>
2022-04-16 20:48:35 -0500

    swap pic and text, css

--- src/containers/Pages/Client/Shopping/Cards/ItemCard/ItemCard.js
+++ src/containers/Pages/Client/Shopping/Cards/ItemCard/ItemCard.js
@@ -1,4 +1,4 @@
-import React, {useState, Fragment} from "react";
+import React, { Fragment, useState } from "react";
 
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -7,13 +7,12 @@ import Typography from "@material-ui/core/Typography";
 import CardActionArea from "@material-ui/core/CardActionArea";
 
 import alt_image from "../../../../../../assets/images/alt_image.jpg";
-import AddToCartCard from "../AddToCartCard/AddToCartCard";
-import TransitionModal from "../../../../../../components/UI/Modal/Modal";
+import SearchModal from "../../../../../../components/UI/Search/SearchModal/SearchModal";
 import * as classes from "./ItemCard.module.css";
 
 const ItemCard = ({item}) => {
     const [modalState, setModalState] = useState(false);
-    const pic = item.picture ? item.picture : alt_image;
+    const pictureUrl = item.pictureUrl ? item.pictureUrl : alt_image;
 
     const toggleModal = () => {
         setModalState(!modalState);
@@ -21,27 +20,23 @@ const ItemCard = ({item}) => {
 
     return (
         <Fragment>
-            <Card key={item.key} className={classes.Card} >
+            <Card key={item.key} className={classes.Card}>
                 <CardActionArea className={classes.Card} onClick={toggleModal} style={{display: 'flex', flexDirection: 'column'}}>
-                    <div className={classes.Title}>
-                        <CardContent >
-                            <Typography gutterBottom variant="h5" component="h2" style={{textAlign: 'center'}}>{item.name}</Typography>
-                        </CardContent>
-                    </div>
                     <div className={classes.Pic}>
                         <CardMedia
-                            component="img"
+                            component={"img"}
                             alt={item.name}
-                            image={pic}/>
+                            image={pictureUrl}
+                        />
+                    </div>
+                    <div className={classes.Title}>
+                        <CardContent >
+                            <Typography gutterBottom variant="h6" style={{textAlign: 'center', wordBreak: 'break-word'}}>{item.name}</Typography>
+                        </CardContent>
                     </div>
                 </CardActionArea>
             </Card>
-            <TransitionModal
-                open={modalState}
-                handleClose={toggleModal}
-                form={<AddToCartCard close={toggleModal} item={item}/>}
-                title={"Item"}
-            />
+            <SearchModal open={modalState} close={toggleModal} item={item}/>
         </Fragment>
     );
 };
