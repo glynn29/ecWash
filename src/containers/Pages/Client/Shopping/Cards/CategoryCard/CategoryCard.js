5188b3623c7c902745a8181b92accc2b02514b93
glynn29 <glynn_29@hotmail.com>
2022-04-16 20:46:57 -0500

    reformat category card

--- src/containers/Pages/Client/Shopping/Cards/CategoryCard/CategoryCard.js
+++ src/containers/Pages/Client/Shopping/Cards/CategoryCard/CategoryCard.js
@@ -1,27 +1,32 @@
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from "@material-ui/core/Typography";
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+
 import alt_image from '../../../../../../assets/images/alt_image.jpg';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
+        height: '100%'
     },
     details: {
         display: 'flex',
         flexDirection: 'column',
     },
     content: {
-        flex: '1 0 auto',
+        flex: '1',
+        height: '100%',
+        display: 'flex', alignItems: 'center', justifyContent: 'center'
     },
     cover: {
         width: 100,
+        height: '100%'
     },
     controls: {
         display: 'flex',
@@ -31,17 +36,15 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-
 const CategoryCard = props => {
     const styles = useStyles();
-
-    const pic = props.picture ? props.picture : alt_image;
+    const pic = props.pictureUrl ? props.pictureUrl : alt_image;
 
     return (
-        <Card key={props.key}>
-            <CardActionArea component={Link} to={'/items/' + props.name} className={styles.root}>
+        <Card key={props.key} style={{height: '100%'}}>
+            <CardActionArea component={Link} to={'/shopping/categories/' + props.name} className={styles.root}>
                 <CardContent className={styles.content}>
-                    <Typography gutterBottom variant="h5" component="h2">{props.name}</Typography>
+                    <Typography gutterBottom variant="h6">{props.name}</Typography>
                 </CardContent>
                 <CardMedia
                     className={styles.cover}
