93ba1ad298492085a89a25ee5c9dfcda59c7c0b4
glynn29 <glynn_29@hotmail.com>
2022-04-16 19:41:45 -0500

    spacing

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,15 +8,15 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "../SideDrawer/SideDrawer.module.css";
 
 const SideDrawer = ({toggleDrawer, drawerState}) => (
-        <Drawer anchor={'left'} open={drawerState} onClose={toggleDrawer} onOpen={toggleDrawer}
-                style={{display: 'flex'}}>
-            <Typography variant="h6" className={classes.Center}>
-                <EcIcon/>
-            </Typography>
-            <div className={classes.List} onClick={toggleDrawer}>
-                <NavigationItems/>
-            </div>
-        </Drawer>
+    <Drawer anchor={'left'} open={drawerState} onClose={toggleDrawer} onOpen={toggleDrawer}
+            style={{display: 'flex'}}>
+        <Typography variant="h6" className={classes.Center}>
+            <EcIcon/>
+        </Typography>
+        <div className={classes.List} onClick={toggleDrawer}>
+            <NavigationItems/>
+        </div>
+    </Drawer>
 );
 
 export default SideDrawer;
