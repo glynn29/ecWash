ad471a54bc534aea5964e4c8a51c0583951f95a9
glynn29 <glynn_29@hotmail.com>
2022-04-16 21:41:18 -0500

    format

--- src/components/Navigation/HeaderCartButton/HeaderCartButton.js
+++ src/components/Navigation/HeaderCartButton/HeaderCartButton.js
@@ -1,16 +1,16 @@
-import Paper from "@material-ui/core/Paper";
-import React, {useEffect, useState} from "react";
-import {connect} from 'react-redux';
-
-import classes from "./HeaderCartButton.module.css";
+import React, { useEffect, useState } from "react";
+import { connect } from 'react-redux';
 
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import Badge from "@material-ui/core/Badge";
 import IconButton from '@material-ui/core/IconButton';
 import { withStyles } from '@material-ui/core/styles';
+
+import classes from "./HeaderCartButton.module.css";
+
 const StyledBadge = withStyles(() => ({
     badge: {
-         color: '#fff', backgroundColor: '#d8222b'
+        color: '#fff', backgroundColor: '#d8222b'
     },
 }))(Badge);
 
@@ -21,7 +21,7 @@ const HeaderCartButton = (props) => {
 
     const numberOfCartItems = (props.items.reduce((curNumber, item) => {
         return curNumber + item.amount;
-    },0));
+    }, 0));
 
     useEffect(() => {
         if (props.items.length === 0) {
@@ -39,19 +39,17 @@ const HeaderCartButton = (props) => {
     }, [props.items]);
 
 
-    return(
+    return (
         <IconButton className={btnClasses2} onClick={props.open}>
-        <StyledBadge badgeContent={numberOfCartItems} showZero >
-            <ShoppingCartIcon className={btnClasses}
-
-                              fontSize={"large"} />
-        </StyledBadge>
+            <StyledBadge badgeContent={numberOfCartItems} showZero>
+                <ShoppingCartIcon className={btnClasses} fontSize={"large"}/>
+            </StyledBadge>
         </IconButton>
     );
 };
 
 const mapStateToProps = state => {
-    return{
+    return {
         items: state.cart.items,
     };
 };
