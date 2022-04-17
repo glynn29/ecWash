4b22b70cd2b877d6f458fedd1fd57ec760c206b1
glynn29 <glynn_29@hotmail.com>
2022-04-16 20:44:58 -0500

    swap pic and text

--- src/containers/Pages/Client/Shopping/Cards/AddToCartCard/AddToCartCard.js
+++ src/containers/Pages/Client/Shopping/Cards/AddToCartCard/AddToCartCard.js
@@ -1,15 +1,13 @@
-import Container from "@material-ui/core/Container";
-import React, {useState} from "react";
-import {connect} from "react-redux";
+import React, { useState } from "react";
+import { connect } from "react-redux";
 
 import Button from "@material-ui/core/Button";
-import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from '@material-ui/core/TextField';
 import FormControl from "@material-ui/core/FormControl";
-
+import Container from "@material-ui/core/Container";
 
 import alt_image from "../../../../../../assets/images/alt_image.jpg";
 import * as actions from "../../../../../../store/actions";
@@ -18,66 +16,57 @@ import * as classes from "../ItemCard/ItemCard.module.css";
 const AddToCartCard = ({item, onAddItem, close}) => {
     const [amount, setAmount] = useState('1');
     const [intAmount, setIntAmount] = useState(1);
-    const pic = item.picture ? item.picture : alt_image;
+    const pictureUrl = (item && item.pictureUrl) ? item.pictureUrl : alt_image;
 
     const handleInputChange = (e) => {
         setAmount(e.target.value);
         setIntAmount(parseInt(e.target.value));
     };
 
-    const actions = (
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
-            <Button style={{width: '45%'}} size={"small"} onClick={() => onAddItem(item, intAmount)} variant={"contained"} color={"secondary"}>Add</Button>
-            <input style={{width: '45%'}} type={"number"} step={1} min={1} max={100} onChange={(e) => handleInputChange(e)} value={amount} />
-        </div>
-    );
-
     const handleSubmit = (e) => {
         e.preventDefault();
         onAddItem(item, intAmount);
+        close();
+        //TODO add snackbar
     };
 
     return (
         <Container component="main" maxWidth="md" style={{textAlign: 'center', padding: 2, width: '90%'}}>
             <form onSubmit={(e) => handleSubmit(e)}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} className={classes.Body}>
-                    <CardContent >
-                        <Typography gutterBottom variant="h5" component="h2" style={{textAlign: 'center'}}>{item.name}</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item xs={12} sm={6} className={classes.Pic}>
-                    <CardMedia
-                        component="img"
-                        alt={item.name}
-                        image={pic}/>
-                </Grid>
-                <Grid container item xs={12} sm={6} spacing={2} style={{margin: '0 auto'}}>
-                    <Grid item xs={12}>
-                        <Typography variant={"body2"}>SKU: {item.code}</Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} className={classes.Pic}>
+                        <CardMedia
+                            component="img"
+                            alt={item && item.name}
+                            image={pictureUrl}/>
                     </Grid>
-                    <Grid item xs={12}>
-                        <Typography variant={"body2"}>Description: {item.details}</Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormControl style={{width: '100%'}}>
-                            <TextField type={"number"}
-                                       onChange={(e) => handleInputChange(e)}
-                                       value={amount} variant={"outlined"}
-                                       label={"Quantity"}
-                                       InputProps={{inputProps:{min: 1, max: 50, step: 1}}} />
-                        </FormControl>
-                    </Grid>
-                    <Grid item sm={6} xs={12}>
-                        <Button onClick={() => close()} variant={"outlined"} style={{width: '100%', color: '#d8222b'}}>Close</Button>
-                    </Grid>
-                    <Grid item sm={6} xs={12}>
-                        <Button type={"submit"} variant={"contained"} color={"primary"} style={{width: '100%'}}>Add to Cart</Button>
+                    <Grid container spacing={2} style={{margin: '0 auto', alignItems: 'end'}}>
+                        <Grid item xs={12}>
+                            <Typography variant={"body2"}
+                                        style={{textAlign: 'left'}}>Description: {item && item.details}</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl style={{width: '100%'}}>
+                                <TextField type={"number"}
+                                           onChange={(e) => handleInputChange(e)}
+                                           value={amount}
+                                           variant={"outlined"}
+                                           label={"Quantity"}
+                                           InputProps={{inputProps: {min: 1, max: 5000, step: 1}}}/>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Button onClick={() => close()} variant={"outlined"}
+                                    style={{width: '100%', color: '#d8222b'}}>Close</Button>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Button type={"submit"} variant={"contained"} color={"primary"} style={{width: '100%'}}>Add
+                                to Cart</Button>
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
             </form>
-        </Container>
+         </Container>
     );
 };
 
