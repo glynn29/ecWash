11107cbae6c8351eb023e2479878c321bec36906
glynn29 <glynn_29@hotmail.com>
2022-04-16 19:59:14 -0500

    firebase config setup, ref for all firebase services

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/functions";
+import "firebase/analytics";
+import "firebase/storage";
+
+const firebaseConfig = JSON.parse(process.env.REACT_APP_PROD_CONFIG);
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+export const auth = firebase.auth();
+export const functions = firebase.functions();
+export const firestore = firebase.firestore();
+export const analytics = firebase.analytics();
+export const storage = firebase.storage();
+export const storageRef = storage.ref();
