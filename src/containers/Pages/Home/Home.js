06e1450b41649087107011321979fd9bf496f808
glynn29 <glynn_29@hotmail.com>
2022-04-23 22:57:21 -0500

    add login, register buttons and css

--- src/containers/Pages/Home/Home.js
+++ src/containers/Pages/Home/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
+import PersonOutlinedIcon from '@material-ui/icons/PersonOutlined';
 
 import * as classes from './Home.module.css';
 import useStyles from "../../../components/UI/Styles/formStyle";
@@ -18,6 +22,29 @@ const Home = () => {
                 <Typography className={styles.homeTagLine}>
                     Your Car Wash Experience Made EC
                 </Typography>
+                <br />
+                <Button
+                    component={Link}
+                    href={"/login"}
+                    variant="contained"
+                    color="primary"
+                    size="medium"
+                    className={classes.Button}
+                    startIcon={<PersonOutlinedIcon  />}
+                >
+                    Login
+                </Button>
+                <Button
+                    component={Link}
+                    href={"/register"}
+                    variant="contained"
+                    color="primary"
+                    size="medium"
+                    className={classes.Button}
+                    startIcon={<PersonAddOutlinedIcon  />}
+                >
+                    Register
+                </Button>
             </div>
         </div>
     );
