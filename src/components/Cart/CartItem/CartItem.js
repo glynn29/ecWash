b21b379517bcc99abbd9e3e5af2c825849bfe547
glynn29 <glynn_29@hotmail.com>
2022-05-04 22:25:15 -0500

    use mui card and add image

--- src/components/Cart/CartItem/CartItem.js
+++ src/components/Cart/CartItem/CartItem.js
@@ -1,24 +1,38 @@
 import React from "react";
 
-import DeleteIcon from '@material-ui/icons/Delete';
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from "@material-ui/core/Typography";
+import DeleteIcon from "@material-ui/icons/Delete";
+import RemoveIcon from "@material-ui/icons/Remove";
+import AddIcon from "@material-ui/icons/Add";
+import Box from "@material-ui/core/Box";
 
+import alt_image from '../../../assets/images/alt_image.jpg';
 import classes from './CartItem.module.css';
 
-const CartItem = (props) => (
-    <li className={classes['cart-item']}>
-        <div>
-            <div className={classes.summary}>
-                <h2>{props.name}</h2>
-                <span className={classes.amount}>x {props.amount}</span>
-            </div>
-        </div>
-        <div className={classes.actions}>
-            <button onClick={props.onRemove} disabled={props.isLoading}>{props.amount === 1 ? <DeleteIcon/> : <RemoveIcon/>}</button>
-            <button onClick={props.onAdd} disabled={props.isLoading}><AddIcon/></button>
-        </div>
-    </li>
-);
+const CartItem = props => {
+    const pic = props.pictureUrl ? props.pictureUrl : alt_image;
+
+    return (
+        <Card key={props.key} className={classes['cart-item']}>
+            <CardMedia
+                className={classes.cover}
+                component="img"
+                alt={props.name}
+                image={pic} />
+            <Box className={classes.content}>
+                <CardContent style={{flex: '1 0 auto'}}>
+                    <Typography variant="h6">{props.name}</Typography>
+                </CardContent>
+                <Box className={classes.actions}>
+                    <button onClick={props.onRemove} disabled={props.isLoading}>{props.amount === 1 ? <DeleteIcon /> : <RemoveIcon />}</button>
+                    <button onClick={props.onAdd} disabled={props.isLoading}><AddIcon /></button>
+                </Box>
+            </Box>
+        </Card>
+    );
+};
 
 export default CartItem;
