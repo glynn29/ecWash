616f79a215d6a9b78bd02840748dcccc6e12f279
glynn29 <glynn_29@hotmail.com>
2022-05-04 22:28:56 -0500

    add cutsheets reducer

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ import authReducer from './store/reducers/auth';
 import cartReducer from './store/reducers/cart';
 import partReducer from './store/reducers/parts';
 import userReducer from './store/reducers/users';
-import categoryReducer from './store/reducers/categories'
+import categoryReducer from './store/reducers/categories';
+import cutSheetReducer from './store/reducers/cutsheets';
 import { AuthProvider } from './containers/Auth/Auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -24,6 +25,7 @@ const rootReducer = combineReducers({
     parts: partReducer,
     categories: categoryReducer,
     users: userReducer,
+    cutSheets: cutSheetReducer,
 });
 
 const store = createStore(rootReducer, composeEnhancers(
