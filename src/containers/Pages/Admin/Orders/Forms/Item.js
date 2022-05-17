db2534e9a4d6691cbc497028795a615adb589ed4
glynn29 <glynn_29@hotmail.com>
2022-05-16 23:07:40 -0500

    add key

--- src/containers/Pages/Admin/Orders/Forms/Item.js
+++ src/containers/Pages/Admin/Orders/Forms/Item.js
@@ -4,7 +4,7 @@ import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
 
 const Item = ({item}) => (
-    <Card style={{padding: '4px'}}>
+    <Card style={{padding: '4px'}} key={item.id}>
         <Typography>{item.name} <span style={{fontWeight: 'bold'}}>X</span> {item.amount}</Typography>
     </Card>
 );
