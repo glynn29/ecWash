36cdeca5a4ffb7b3cf4135ac0e0db85f65ccf4d4
glynn29 <glynn_29@hotmail.com>
2022-05-31 22:31:09 -0500

    spacing, add new tabs for customers

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -10,26 +10,28 @@ import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import CategoryIcon from '@material-ui/icons/Category';
-import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import BurstModeIcon from '@material-ui/icons/BurstMode';
 import CropFreeIcon from '@material-ui/icons/CropFree';
 import HistoryIcon from '@material-ui/icons/History';
+import DescriptionIcon from '@material-ui/icons/Description';
+import BuildIcon from '@material-ui/icons/Build';
 import Divider from "@material-ui/core/Divider";
 
 import NavigationItem from "./NavagationItem/NavigationItem";
 
 const NavigationItems = () => {
-    const {currentUser, isAdmin} = useContext(AuthContext);
+    const { currentUser, isAdmin } = useContext(AuthContext);
 
     const nav = currentUser ?
-        <NavigationItem link="/logout" text="Logout" icon={<ArrowBackIosIcon/>}/>
+        <NavigationItem link="/logout" text="Logout" icon={<ArrowBackIosIcon />} />
         :
-        <NavigationItem link="/login" text="Login" icon={<ArrowForwardIosIcon/>}/>;
+        <NavigationItem link="/login" text="Login" icon={<ArrowForwardIosIcon />} />;
 
     let links = (
         <List>
             {nav}
-            <Divider/>
-            <NavigationItem link="/" exact text="Home" icon={<HomeIcon/>}/>
+            <Divider />
+            <NavigationItem link="/" exact text="Home" icon={<HomeIcon />} />
         </List>
     );
 
@@ -38,25 +40,30 @@ const NavigationItems = () => {
             links = (
                 <List>
                     {nav}
-                    <Divider/>
-                    <NavigationItem link="/orders" text="Orders" icon={<HistoryIcon/>}/>
-                    <NavigationItem link="/users" text="Users" icon={<PersonIcon/>}/>
-                    <NavigationItem link="/parts" text="Parts" icon={<WorkOutlineIcon/>}/>
-                    <NavigationItem link="/category" text="Category" icon={<CategoryIcon/>}/>
-                    <Divider/>
-                    <NavigationItem link="/shopping" text="Shopping" icon={<ShoppingCartIcon/>}/>
-                    <Divider/>
-                    <NavigationItem link="/qr" text="QR Manager" icon={<CropFreeIcon/>}/>
-                    <NavigationItem link="/kits" text="Kits" icon={<AccessAlarmIcon/>}/>
+                    <Divider />
+                    <NavigationItem link="/orders" text="Orders" icon={<HistoryIcon />} />
+                    <NavigationItem link="/users" text="Users" icon={<PersonIcon />} />
+                    <NavigationItem link="/parts" text="Parts" icon={<WorkOutlineIcon />} />
+                    <NavigationItem link="/category" text="Categories" icon={<CategoryIcon />} />
+                    <Divider />
+                    <NavigationItem link="/shopping" text="Shopping" icon={<ShoppingCartIcon />} />
+                    <NavigationItem link="/supportdocuments" text="Tech Support Documents" icon={<DescriptionIcon />} />
+                    <NavigationItem link="/maintenance" text="Recommended Maintenance" icon={<BuildIcon />} />
+                    <Divider />
+                    <NavigationItem link="/qr" text="QR Manager" icon={<CropFreeIcon />} />
+                    <NavigationItem link="/kits" text="Mass Picture Upload" icon={<BurstModeIcon />} />
                 </List>
             )
         } else {
             links = (
                 <List>
                     {nav}
-                    <Divider/>
-                    <NavigationItem link="/shopping" text="Shopping" icon={<ShoppingCartIcon/>}/>
-                    <NavigationItem link="/orderhistory" text="Orders History" icon={<HistoryIcon/>}/>
+                    <Divider />
+                    <NavigationItem link="/shopping" text="Shopping" icon={<ShoppingCartIcon />} />
+                    <NavigationItem link="/orderhistory" text="Orders History" icon={<HistoryIcon />} />
+                    <Divider />
+                    <NavigationItem link="/supportdocuments" text="Tech Support Documents" icon={<DescriptionIcon />} />
+                    <NavigationItem link="/maintenance" text="Recommended Maintenance" icon={<BuildIcon />} />
                 </List>
             );
         }
