85dd9677596f39175511755e3eed9cf09fdd0c5e
glynn29 <glynn_29@hotmail.com>
2022-06-01 19:31:13 -0500

    resources page for managers

---
+++ src/containers/Pages/Client/ManagerResources/ManagerResources.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Grow from "@material-ui/core/Grow";
+
+import ItemCard from "../CutSheets/Cards/ItemCard";
+import EC_Recommended_Maintenance from "../../../../assets/files/Manager Resources/EC_Recommended_Maintenance.pdf";
+
+const ManagerResources = () => {
+    const item = {
+        name: "Recommended Maintenance",
+        file: "EC_Recommended_Maintenance"
+    }
+
+    return (
+        <Container>
+            <Grid container spacing={2}>
+                <Grow in={true} key={item.name} style={{ transformOrigin: '0 0 0' }} {...{ timeout: 100 }}>
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <ItemCard item={item} pdfs={[EC_Recommended_Maintenance]} />
+                    </Grid>
+                </Grow>
+            </Grid>
+        </Container>
+    );
+};
+
+export default ManagerResources;
