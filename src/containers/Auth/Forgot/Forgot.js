cb7be4f3d57b85cc39b5b239e83572e3b96a6e2f
glynn29 <glynn_29@hotmail.com>
2022-06-06 22:23:33 -0500

    update forgot password style, and use correct resetUrl

--- src/containers/Auth/Forgot/Forgot.js
+++ src/containers/Auth/Forgot/Forgot.js
@@ -4,58 +4,103 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Avatar from "@material-ui/core/Avatar";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Grid from "@material-ui/core/Grid";
+import FormControl from "@material-ui/core/FormControl";
+import Link from "@material-ui/core/Link";
 
 import formStyles from "../../../components/UI/Styles/formStyle";
-import { auth } from "../../../firebase";
+import { auth, passwordResetUrl } from "../../../firebase";
 
 const Forgot = () => {
     const [email, setEmail] = useState("");
-    const [result, setResult] = useState(null);
+    const [result, setResult] = useState("");
+    const [error, setError] = useState("");
+    const [emailError, setEmailError] = useState("");
     const classes = formStyles();
 
     const submitHandler = (event) => {
         event.preventDefault();
-        const actionCodeSettings = {
-            //TODO change this url
-            url: 'https://carwash-57347.web.app/login',
-            handleCodeInApp: false
-        };
 
-        auth.sendPasswordResetEmail(email, actionCodeSettings)
-            .then(() => setResult("Successfully sent reset email to: " + email))
-            .catch(error => setResult("Error sending Email: " + error.message));
+        const validEmail = /\S+@\S+\.\S+/;
+        let isValid = true;
+        if (!email.match(validEmail)) {
+            setEmailError("Poorly formatted email");
+            isValid = false;
+        }
+
+        if (isValid) {
+            setEmailError("");
+            const actionCodeSettings = {
+                url: passwordResetUrl,
+                handleCodeInApp: false
+            };
+
+            auth.sendPasswordResetEmail(email, actionCodeSettings)
+                .then(() => setResult("Successfully sent reset email to: " + email + ". If you dont see it within a minute, check your junk folder."))
+                .catch(error => setError("Error sending Email: " + error.message));
+        }
     };
 
     return (
         <Container component="main" maxWidth="sm">
             <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
                 <Typography component="h1" variant="h5">
                     Forgot Password
                 </Typography>
-                {result}
-                <form className={classes.form} noValidate onSubmit={submitHandler}>
-                    <TextField
-                        value={email}
-                        onChange={event => setEmail(event.target.value)}
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        SUBMIT
-                    </Button>
+                <br />
+                <form onSubmit={submitHandler}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <FormControl className={classes.formControl}>
+                                <TextField
+                                    value={email}
+                                    onChange={event => setEmail(event.target.value)}
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    {...(emailError && { error: true, helperText: emailError })}
+                                    autoFocus
+                                />
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography color={"error"}>
+                                {error}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography>
+                                {result}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                            >
+                                Reset Password
+                            </Button>
+                        </Grid>
+                        <Grid item xs>
+                            <Link href="/login" variant="body2">
+                                Login
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="/register" variant="body2">
+                                Don't have an account? Register
+                            </Link>
+                        </Grid>
+                    </Grid>
                 </form>
             </div>
         </Container>
