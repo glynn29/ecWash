33221e0bc1a73465fb91fab0c1f58336b63043b8
glynn29 <glynn_29@hotmail.com>
2022-07-09 21:05:26 -0500

    update form for adding users

--- src/containers/Pages/Admin/Users/Forms/Add/Add.js
+++ src/containers/Pages/Admin/Users/Forms/Add/Add.js
@@ -1,71 +1,101 @@
-import React, {useState} from "react";
+import React, { useContext, useState } from "react";
 
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Divider from "@material-ui/core/Divider";
-import FormControl from "@material-ui/core/FormControl";
 import Grid from "@material-ui/core/Grid";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 
 import formStyles from "../../../../../../components/UI/Styles/formStyle";
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-    PaperProps: {
-        style: {
-            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-            width: 250,
-        },
-    },
-};
+import { AuthContext } from "../../../../../Auth/Auth";
 
 const Add = props => {
-    const [first, setFirst] = useState("");
-    const [last, setLast] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [city, setCity] = useState("");
-    const [state, setState] = useState("");
-    const [zip, setZip] = useState("");
-    const [locationPhone, setLocationPhone] = useState("");
-    const [managerPhone, setManagerPhone] = useState("");
-    const [washType, setWashType] = useState("");
-    const [otherWashType, setOtherWashType] = useState("");
+    const [first, setFirst] = useState("glynn");
+    const [last, setLast] = useState("poop");
+    const [email, setEmail] = useState("a@a.com");
+    const [password, setPassword] = useState("123456");
+    const [city, setCity] = useState("dsfs");
+    const [state, setState] = useState("sd");
+    const [zip, setZip] = useState("12322");
+    const [address, setAddress] = useState("asdf s");
+    const [locationPhone, setLocationPhone] = useState("1234567890");
+    const [managerPhone, setManagerPhone] = useState("1234567890");
+    const [nickName, setNickName] = useState("");
     const [error, setError] = useState({});
-    const washTypeList = [{name: 'Tommy\'s'}, {name: 'Zips'}, {name: 'Other'}];
+    const { isAdmin } = useContext(AuthContext);
     const classes = formStyles();
+
+    const formValidator = () => {
+        let tempErrors = {};
+        let isValid = true;
+        const letters = /^[A-Za-z\s]+$/;
+        const validEmail = /\S+@\S+\.\S+/;
+
+        if (!first.match(letters)) {
+            tempErrors.first = "Name must only contain letters";
+            isValid = false
+        }
+
+        if (first.trim() === "") {
+            tempErrors.first = "Name must not be empty";
+            isValid = false
+        }
+
+        if (!last.match(letters)) {
+            tempErrors.last = "Name must only contain letters";
+            isValid = false
+        }
+
+        if (last.trim() === "") {
+            tempErrors.last = "Name must not be empty";
+            isValid = false
+        }
+
+        if (!email.match(validEmail)) {
+            tempErrors.email = "Poorly formatted email";
+            isValid = false;
+        }
+
+        if (password.length < 6) {
+            tempErrors.password = "Password must be at least 6 characters";
+            isValid = false;
+        }
+
+        setError({...tempErrors});
+        return isValid;
+    };
 
     const submitHandler = (event) => {
         event.preventDefault();
-        const tempFirst = first.trim()
-            .charAt(0)
-            .toUpperCase() + first.trim()
-            .slice(1);
-        const tempLast = last.trim()
-            .charAt(0)
-            .toUpperCase() + last.trim()
-            .slice(1);
+        if (formValidator()) {
+            const trimmedFirst = first.trim()
+                .charAt(0)
+                .toUpperCase() + first.trim()
+                .slice(1);
+            const trimmedLast = last.trim()
+                .charAt(0)
+                .toUpperCase() + last.trim()
+                .slice(1);
 
-        const formData = {
-            email: email,
-            city: city,
-            state: state,
-            zip: zip,
-            locationPhone: locationPhone,
-            washType: washType,
-            first: tempFirst,
-            last: tempLast,
-            managerPhone: managerPhone,
-            password: password,
-        };
+            const formData = {
+                email: email.trim(),
+                password: password,
+                city: city.trim(),
+                state: state.trim()
+                    .toUpperCase(),
+                zip: zip.trim(),
+                address: address.trim(),
+                locationPhone: locationPhone.trim(),
+                first: trimmedFirst,
+                last: trimmedLast,
+                managerPhone: managerPhone.trim(),
+                nickName: nickName,
+                approved: "true"
+            };
 
-        props.onAdd(formData);
-
+            props.onAdd(formData, isAdmin);
+        }
     };
     return (
         <Container component="main" maxWidth="sm">
@@ -73,7 +103,7 @@ const Add = props => {
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <Typography>Credentials</Typography>
-                        <Divider/>
+                        <Divider />
                     </Grid>
                     <Grid item xs={12}>
                         <TextField
@@ -86,7 +116,7 @@ const Add = props => {
                             id="email"
                             label="Email Address"
                             autoFocus
-                            {...(error.email && {error: true, helperText: error.email})}
+                            {...(error.email && { error: true, helperText: error.email })}
                         />
                     </Grid>
                     <Grid item xs={12}>
@@ -100,12 +130,12 @@ const Add = props => {
                             label="Password"
                             type="password"
                             id="password"
-                            {...(error.password && {error: true, helperText: error.password})}
+                            {...(error.password && { error: true, helperText: error.password })}
                         />
                     </Grid>
                     <Grid item xs={12}>
-                        <Typography>Location ID</Typography>
-                        <Divider/>
+                        <Typography>Car Wash Location Info</Typography>
+                        <Divider />
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <TextField
@@ -116,7 +146,7 @@ const Add = props => {
                             fullWidth
                             id="city"
                             label="City"
-                            {...(error.email && {error: true, helperText: error.email})}
+                            {...(error.email && { error: true, helperText: error.email })}
                         />
                     </Grid>
                     <Grid item xs={12} sm={2}>
@@ -143,6 +173,18 @@ const Add = props => {
                     </Grid>
                     <Grid item xs={12}>
                         <TextField
+                            value={address}
+                            onChange={event => setAddress(event.target.value)}
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="address"
+                            label="Address"
+                            inputProps={{ maxLength: 63 }}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
                             value={locationPhone}
                             onChange={event => setLocationPhone(event.target.value)}
                             variant="outlined"
@@ -153,41 +195,8 @@ const Add = props => {
                         />
                     </Grid>
                     <Grid item xs={12}>
-                        <FormControl variant="outlined" className={classes.formControl} required>
-                            <InputLabel>Wash Type</InputLabel>
-                            <Select
-                                MenuProps={MenuProps}
-                                value={washType}
-                                onChange={event => setWashType(event.target.value)}
-                                label="Wash Type"
-                            >
-                                {washTypeList.map(listItem => {
-                                    return (
-                                        <MenuItem key={listItem.name}
-                                                  value={listItem.name}>{listItem.name}</MenuItem>
-                                    );
-                                })}
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    {washType === "Other" &&
-                    <Grid item xs={12}>
-                        <FormControl variant="outlined" className={classes.formControl}>
-                            <TextField
-                                value={otherWashType}
-                                onChange={event => setOtherWashType(event.target.value)}
-                                label="Wash Type"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="otherWashType"
-                                {...(error.role && {error: true, helperText: error.role})}
-                            />
-                        </FormControl>
-                    </Grid>}
-                    <Grid item xs={12}>
-                        <Typography>General Manager Contact</Typography>
-                        <Divider/>
+                        <Typography>General Manager Contact Info</Typography>
+                        <Divider />
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <TextField
@@ -198,7 +207,7 @@ const Add = props => {
                             fullWidth
                             id="firstName"
                             label="First Name"
-                            {...(error.first && {error: true, helperText: error.first})}
+                            {...(error.first && { error: true, helperText: error.first })}
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
@@ -210,7 +219,7 @@ const Add = props => {
                             fullWidth
                             id="lastName"
                             label="Last Name"
-                            {...(error.last && {error: true, helperText: error.last})}
+                            {...(error.last && { error: true, helperText: error.last })}
                         />
                     </Grid>
                     <Grid item xs={12}>
@@ -222,6 +231,17 @@ const Add = props => {
                             fullWidth
                             id="mobile"
                             label="Phone Number"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            value={nickName}
+                            onChange={event => setNickName(event.target.value)}
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="nickname"
+                            label="Nickname"
                         />
                     </Grid>
                     <Grid item xs={12}>
